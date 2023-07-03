d7a58ec4ce906d230952fa970a8961f1b9976c5a
javier-aguilar <jvr.agx@gmail.com>
2023-07-03 15:08:15 -0600

    Add user and user log

---
+++ db/migrate/20230703204943_create_user_logs.rb
@@ -0,0 +1,12 @@
+class CreateUserLogs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_logs do |t|
+      t.references :user, null: false, foreign_key: true
+      t.float :weight
+      t.float :body_fat
+      t.float :muscle_mass
+
+      t.timestamps
+    end
+  end
+end
