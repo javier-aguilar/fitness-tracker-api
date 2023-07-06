2e4510176a883308506d799c0921b9df2e88cb28
javier-aguilar <jvr.agx@gmail.com>
2023-07-06 15:33:43 -0600

    Add validation

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,8 @@
 class Log < ApplicationRecord
+  validates_presence_of :user_id,
+                        :weight,
+                        :body_fat,
+                        :muscle_mass
   belongs_to :user
+  default_scope { order(created_at: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+    validates_presence_of :first_name,
+                          :last_name,
+                          :dob
     has_many :logs
 end
