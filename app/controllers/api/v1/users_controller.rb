173e86ab84ed6e283e7021aabd1e81c53087bd2e
javier-aguilar <jvr.agx@gmail.com>
2023-07-05 22:19:05 -0600

    Add user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: {status: "SUCCESS", message: "Fetched all users successfully", data: users}, status: :ok
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: {status: "SUCCESS", message: "Fetched user successfully", data: user}, status: :ok
+    end
 end
